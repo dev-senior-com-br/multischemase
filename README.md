ff93ea5de6359646762530c564015fcc3fa8c76e
andre.rezende <andre.rezende@senior.com.br>
2020-06-10 10:57:39 -0300

    Initial project in JS.

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
-# Multischemase
+Multischemase
+================
+
 Multischemase é uma ferramenta de criação de banco de dados multi schemas e migração independente de estrutura para o Node.
 
+## Depêndencias
+Esse projeto tem depêndencia de JAVA +1.8 e com variável de ambiente configurada JAVA_HOME.
+
+## Suporte
+Esse projeto poderá ser executado nos seguintes sistemas operacionais que estão testados: Windows 10, Linux e Mac OS X
+
+## Instalar
+Na raíz do projeto executar a linha de comando:
+```
+npm install
+```
+
+### Executar por linha de comando
+Para executar a migração por linha de comando, digite:
+```
+node .\bin\flyway.js -c conf/config.js migrate
+```
+
+#### Funcionalidades
+|Parâmetro|Descrição|
+|-----|----|--------|-----------|
+|migrate|Migra o schema para a última versão.O Flyway irá criar a tabela de metadata automaticamente se ela não existir.|
+|clean|Apaga todos objetos (tables, views, procedures, triggers, ...) nos schemas configurados. Os schemas são limpos na ordem especificadas pelas propriedades dos schemas.|
+|info|Imprime os detalhes e informações de status sobre todas as migrações.|
+|baseline|Baselines de um database existente, excluindo todas migrações executadas e incluidas em baselineVersion.|
+|repair|Repara a tabela de metadata do Flyway. Irá executar as seguintes ações:<br> - Remover qualquer migração falhada no banco de dados sem transação de DDL(Objetos do usuário deixados para trás dever ser limpos manualmente)<br> - Corrigir checksums incorretos|
+|validate|Valida migrações aplicadas com as encontradas (no sistema de arquivos ou classpath) para detectar alterações acidentais possibilitando que os schemas sejam criados exatamente como desejado.A validação falhará se:<br>- diferenças nos nomes das migrações, tipos or checksums encontrados<br>- versões aplicadas que não podem ser resolvidas localmente<br>- versões resolvidas que não foram aplicadas ainda|
+			 
+## Configurações
+As configurações poderão ser realizas por arquivo JS ou JSON. Na pasta `conf` possui um exemplo em Postgres local de nome `config.js`.
+A função pede dois parâmetros:<br>
+O nome do serviço pela variável `service`;<br>
+O nome do tenant pela variável `tenant`.
+Para configurar a conexão com o seu banco, altere as propriedades conforme abaixo:
+```
+        flywayArgs: {
+            url: '<JDBC_BANCO_DADOS>' //ex:jdbc:postgresql://localhost/postgres,
+            ..,
+            user: '<NOME_DO_USUARIO_BANCO_DADOS>' //postgres,
+            password: '<SENHA_DO_USUARIO_BANCO_DADOS>' //postgres,
+           ...
+        }
+```
+Os arquivos de migração devem ficar localizados na pasta `db/sql` e a extensão deles deverá ser `.pgsql`.
+Vide exemplo de arquivo em  `V0001__StartingOut.pgsql` que criará uma função de nome _next_id no schema public do seu banco Postgres. 
 
 ## Compilar o projeto
 
---
+++ bin/flyway.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const program = require('commander');
+const pkg = require('../package.json');
+const path = require('path');
+const spawn = require('child_process').spawn;
+const download = require('../lib/download');
+
+process.title = 'flyway';
+program
+    .version(pkg.version)
+    .option('-c, --configfile <file>', 'Um jscript ou arquivo JSON contendo as configurações.')
+    .on('--help', function() {
+        console.log(' Para verificar as opções de configuração do Flyway\'s: https://flywaydb.org/documentation/commandline/');
+    });
+
+makeCommand('migrate', 'Migra o schema para a última versão.O Flyway irá criar a tabela de metadata automaticamente se ela não existir.');
+makeCommand('clean', 'Apaga todos objetos (tables, views, procedures, triggers, ...) nos schemas configurados. Os schemas são limpos na ordem especificadas pelas propriedades dos schemas.');
+makeCommand('validate', `Valida migrações aplicadas com as encontradas (no sistema de arquivos ou classpath) para detectar alterações acidentais possibilitando que os schemas sejam criados exatamente como desejado.
+
+            A validação falhará se
+             - diferenças nos nomes das migrações, tipos or checksums encontrados
+             - versões aplicadas que não podem ser resolvidas localmente
+             - versões resolvidas que não foram aplicadas ainda`);
+makeCommand('baseline', 'Baselines de um database existente, excluindo todas migrações executadas e incluidas em baselineVersion.');
+makeCommand('repair', `Repara a tabela de metadata do Flyway. Irá executar as seguintes ações:
+
+             -Remover qualquer migração falhada no banco de dados sem transação de DDL
+             (Objetos do usuário deixados para trás dever ser limpos manualmente)
+             - Corrigir checksums incorretos`);
+
+program.parse(process.argv);
+
+function makeCommand(name, desc) {
+    program
+        .command(name)
+        .description(desc)
+        .action(exeCommand);
+}
+
+function configFlywayArgs(config) {
+    const flywayArgs = config.flywayArgs || {};
+    const flywayArgsKeys = Object.keys(flywayArgs);
+
+    return flywayArgsKeys.map(function(key) {
+        return `-${key}=${flywayArgs[key]}`;
+    });
+}
+
+function exeCommand(cmd) {
+    if(!program.configfile) {
+        throw new Error('Arquivo de configurações é requerido, ver https://github.com/dev-senior-com-br/multischemase/blob/master/README.md');
+    }
+
+    var config = require(path.resolve(program.configfile));
+
+    if (typeof config === 'function') {
+        var service = 'service';
+        var tenant = 'teste';
+        config = config(service, tenant);
+    }
+
+    download.ensureArtifacts(config, function(err, flywayBin) {
+        const workingDir = process.cwd();
+
+        if(err) {
+            throw new Error(err);
+        }
+
+        const args = configFlywayArgs(config)
+            .concat([cmd._name]);
+
+        const child = spawn(flywayBin, args, {
+            env: Object.assign({}, process.env, config.env),
+            cwd: workingDir,
+            stdio: 'inherit',
+            windowsVerbatimArguments: true // Super Weird, https://github.com/nodejs/node/issues/5060
+        });
+
+        child.on('close', code => {
+            process.exit(code);
+        });
+    });
+}
